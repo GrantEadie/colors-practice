5348d5ef4a8b34097075fbc3f5b6fbd03a11ba11
Grant Eadie <grantleadie@gmail.com>
2020-08-11 11:24:37 -0700

    I can't get this to work and its killing me.

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  $("button#green").click(function() {
+    $("body").removeClass();
+    $("body").addClass("green-background");
+  });
+
+  $("button#yellow").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
+  });
+
+  $("button#red").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
+  });
+
+  $("p").click(function() {
+    $("p").addClass("border-add");
+  });
+});
